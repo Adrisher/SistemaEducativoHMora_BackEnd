b555ddabb1de3ab7b7053576c84d28f7cec9e926
LChumi <luischumi.9@gmail.com>
2023-02-06 21:41:53 -0500

    update

---
+++ src/main/java/com/ista/backend/mapper/ProfesorDTOToProfesor.java
@@ -0,0 +1,25 @@
+package com.ista.backend.mapper;
+
+import com.ista.backend.persistence.entity.Profesor;
+import com.ista.backend.service.dto.ProfesorDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProfesorDTOToProfesor implements IMapper<ProfesorDTO, Profesor> {
+    @Override
+    public Profesor map(ProfesorDTO in) {
+        Profesor profesor= new Profesor();
+        profesor.setCedula(in.getCedula());
+        profesor.setNombre(in.getNombre());
+        profesor.setSegundo_nombre(in.getSegundo_nombre());
+        profesor.setPrimer_apellido(in.getPrimer_apellido());
+        profesor.setSegundo_apellido(in.getSegundo_apellido());
+        profesor.setGenero(in.getGenero());
+        profesor.setFecha_nacimiento(in.getFecha_nacimiento());
+        profesor.setCorreo(in.getCorreo());
+        profesor.setEstado(true);
+        profesor.setArea(in.getArea());
+        profesor.setCv(null);
+        return profesor;
+    }
+}
---
+++ src/main/java/com/ista/backend/service/dto/ProfesorDTO.java
@@ -0,0 +1,22 @@
+package com.ista.backend.service.dto;
+
+import com.ista.backend.persistence.enums.SexoStatus;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class ProfesorDTO {
+
+    private String cedula;
+    private String nombre;
+    private String segundo_nombre;
+    private String primer_apellido;
+    private String segundo_apellido;
+    private SexoStatus genero;
+    private Date fecha_nacimiento;
+    private String correo;
+    private String direccion;
+    private String area;
+    private String contraseña;
+}
